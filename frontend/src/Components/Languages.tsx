61b5ee75bbdd11a2dd36f96c8b1ff05ea5a0ffaf
XPS <ashvinshash@gmail.com>
2021-03-22 13:42:33 +0100

    Change ternary operators to IF statement.

--- frontend/src/Components/Languages.tsx
+++ frontend/src/Components/Languages.tsx
@@ -26,11 +26,16 @@ interface Language {
 const Languages = ({ org, repoName }: IProps) => {
     const url = buildUrl(API.LANGUAGES, { org, repoName });
     const { data, loading, error }: Response = useFetch({}, url);
-    return loading ? (
-        <CirclesToRhombusesSpinner color="brand" />
-    ) : error ? (
-        <ErrorMsg />
-    ) : (
+
+    if (loading) {
+        return <CirclesToRhombusesSpinner color="brand" />;
+    }
+
+    if (error) {
+        return <ErrorMsg />;
+    }
+
+    return (
         <Card pad="small" width="small" height="small" background="light-1" elevation="large">
             <CardHeader alignSelf="start" pad="xsmall">
                 <Heading level="4" margin="none">
@@ -48,6 +53,29 @@ const Languages = ({ org, repoName }: IProps) => {
             </CardBody>
         </Card>
     );
+
+    // return loading ? (
+    //     <CirclesToRhombusesSpinner color="brand" />
+    // ) : error ? (
+    //     <ErrorMsg />
+    // ) : (
+    //     <Card pad="small" width="small" height="small" background="light-1" elevation="large">
+    //         <CardHeader alignSelf="start" pad="xsmall">
+    //             <Heading level="4" margin="none">
+    //                 Top Languages
+    //             </Heading>
+    //         </CardHeader>
+    //         <CardBody wrap alignSelf="start" alignContent="start" pad="xsmall">
+    //             <ul>
+    //                 {Object.keys(data)
+    //                     .splice(0, 3)
+    //                     .map((lan: string) => (
+    //                         <li key={lan}>{lan}</li>
+    //                     ))}
+    //             </ul>
+    //         </CardBody>
+    //     </Card>
+    // );
 };
 
 export default Languages;
