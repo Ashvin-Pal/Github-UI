bd024c5aef62e2f76932b5e37d40f46540911a52
Ashvin-Pal <ashvinshash@gmail.com>
2021-08-29 12:17:17 +0200

    add theme file

---
+++ frontend/src/theme.tsx
@@ -0,0 +1,12 @@
+export const APP_THEME = {
+    global: {
+        colors: {
+            brand: "#000000",
+        },
+        font: {
+            family: "Roboto",
+            size: "18px",
+            height: "20px",
+        },
+    },
+};
